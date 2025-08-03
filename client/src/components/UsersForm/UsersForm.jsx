e306a7ef5e1af1aca0069e2c8b2d627acd290e3e
shanthi1710 <shanthinathshedabale@gmail.com>
2025-08-03 23:59:06 +0530

    add user management components and service integration

--- client/src/components/UsersForm/UsersForm.jsx
+++ client/src/components/UsersForm/UsersForm.jsx
@@ -1,19 +1,58 @@
-import React from 'react'
+import {useState} from "react";
+import {addUser} from "../../Service/UserService.js";
+import toast from "react-hot-toast";
 
-export default function UsersForm() {
-  return (
-    <div className="mx-2 mt-2">
+const UsersForm = ({setUsers}) => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        role: "ROLE_USER"
+    });
+
+    const onChangeHandler = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        setData((data) => ({ ...data, [name]: value }));
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const response = await addUser(data);
+            setUsers((prevUsers) => [...prevUsers, response.data]);
+            toast.success("User Added");
+            setData({
+                name: "",
+                email: "",
+                password: "",
+                role: "ROLE_USER",
+            })
+        } catch (e) {
+            console.error(e);
+            toast.error("Error adding user");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <div className="mx-2 mt-2">
             <div className="row">
                 <div className="card col-md-12 form-container">
                     <div className="card-body">
-                        <form >
+                        <form onSubmit={onSubmitHandler}>
                             <div className="mb-3">
                                 <label htmlFor="name" className="form-label">Name</label>
                                 <input type="text"
                                        name="name"
                                        id="name"
                                        className="form-control"
-                                       placeholder="Your Name"
+                                       placeholder="Jhon Doe"
+                                       onChange={onChangeHandler}
+                                       value={data.name}
                                        required
                                 />
                             </div>
@@ -24,6 +63,8 @@ export default function UsersForm() {
                                        id="email"
                                        className="form-control"
                                        placeholder="yourname@example.com"
+                                       onChange={onChangeHandler}
+                                       value={data.email}
                                        required
                                 />
                             </div>
@@ -34,16 +75,20 @@ export default function UsersForm() {
                                        id="password"
                                        className="form-control"
                                        placeholder="**************"
+                                       onChange={onChangeHandler}
+                                       value={data.password}
                                        required
                                 />
                             </div>
-                            <button type="submit" className="btn btn-warning w-100" >
-                                Submit
+                            <button type="submit" className="btn btn-warning w-100" disabled={loading}>
+                                {loading ? "Loading..." : "Save"}
                             </button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-  )
+    )
 }
+
+export default UsersForm;
