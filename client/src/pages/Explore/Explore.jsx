5c5e5816c4a42c44e3fdf0ee2c68c31f93725904
shanthi1710 <shanthinathshedabale@gmail.com>
2025-08-04 14:58:25 +0530

    add CartItems, CartSummary, DisplayItems, and CustomerForm components with styles

--- client/src/pages/Explore/Explore.jsx
+++ client/src/pages/Explore/Explore.jsx
@@ -1,32 +1,51 @@
-import { useContext } from 'react';
-import { AppContext } from '../../context/AppContext';
 import './Explore.css';
-
+import {useContext, useState} from "react";
+import {AppContext} from "../../context/AppContext.jsx";
+import DisplayCategory from "../../components/DisplayCategory/DisplayCategory.jsx";
+import DisplayItems from "../../components/DisplayItems/DisplayItems.jsx";
+import CustomerForm from "../../components/CustomerForm/CustomerForm.jsx";
+import CartItems from "../../components/CartItems/CartItems.jsx";
+import CartSummary from "../../components/CartSummary/CartSummary.jsx";
 
 const Explore = () => {
     const {categories} = useContext(AppContext);
-    console.log(categories);
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [customerName, setCustomerName] = useState("");
+    const [mobileNumber, setMobileNumber] = useState("");
     return (
         <div className="explore-container text-light">
             <div className="left-column">
                 <div className="first-row" style={{overflowY: 'auto'}}>
-                     Categories
+                    <DisplayCategory
+                        selectedCategory={selectedCategory}
+                        setSelectedCategory={setSelectedCategory}
+                        categories={categories} />
                 </div>
                 <hr className="horizontal-line" />
                 <div className="second-row" style={{overflowY: 'auto'}}>
-                     items
+                    <DisplayItems selectedCategory={selectedCategory} />
                 </div>
             </div>
             <div className="right-column d-flex flex-column">
                 <div className="customer-form-container" style={{height: '15%'}}>
-                     customer form
+                    <CustomerForm
+                        customerName={customerName}
+                        mobileNumber={mobileNumber}
+                        setMobileNumber={setMobileNumber}
+                        setCustomerName={setCustomerName}
+                    />
                 </div>
                 <hr className="my-3 text-light" />
                 <div className="cart-items-container" style={{height: '55%', overflowY: 'auto'}}>
-                    cart items 
+                    <CartItems />
                 </div>
                 <div className="cart-summary-container" style={{height: '30%'}}>
-                     cart summary
+                    <CartSummary
+                        customerName={customerName}
+                        mobileNumber={mobileNumber}
+                        setMobileNumber={setMobileNumber}
+                        setCustomerName={setCustomerName}
+                    />
                 </div>
             </div>
         </div>
